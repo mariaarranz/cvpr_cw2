2a9702ea87c4000e6024930c4ba54cbb26d89594
patmccarthy1 <pat.mccarthy1@aol.com>
2021-03-11 17:36:01 +0000

    added start of LDA code

--- cvpr_coursework_2.m
+++ cvpr_coursework_2.m
@@ -7,7 +7,7 @@
 
 % load data
 dir_name = 'PR_CW_DATA_2021';
-file_name = 'car_sponge_101_08_HOLD'; % file name - change this to load different files
+file_name = 'kitchen_sponge_114_02_HOLD'; % file name - change this to load different files
 load(strcat(dir_name,'/',file_name,'.mat')) 
 
 % plot pressure
@@ -58,11 +58,11 @@ title(['Electrode Impedance for ',file_name],'Interpreter', 'none')
 
 % 2 - Sample data for one finger
 
-timestep = 500;                                 % sample to use for comparison
-PVT = struct;                                  %  struct for data for all materials at chosen timestep
+timestep = 612;                                 % sample to use for comparison
+PVT = struct;                                   %  struct for data for all materials at chosen timestep
 Electrodes = struct;
 myDir = dir_name;
-myFiles = dir(fullfile(dir_name,'*.mat'));      % get all mat files in struct
+myFiles = dir(fullfile(dir_name,'*.mat'));      % get all mat files in directory
 for k = 1:length(myFiles)
   baseFileName = myFiles(k).name;
   fullFileName = fullfile(myDir, baseFileName);
@@ -94,15 +94,110 @@ end
 
 % scatter plot
 figure
-colours = linspace(1,10,length(pressure));
-scatter3(pressure,vibration,temperature,10,colours,'filled')
+colours = linspace(1,1000,length(pressure));
+scatter3(pressure,vibration,temperature,20,colours,'filled')
 xlabel('pressure')
 ylabel('vibration')
 zlabel('temperature')
 title('Scatter Plot for PVT at timestep 500')
 
 %% Section B - Principal Component Analysis
+A = [pressure;vibration;temperature];
+C = cov(A);
 
 %% Section C - Linear Discriminant Analysis
+
+% isolate data for black foam and car sponge
+
+names = [];
+pressure = [];
+vibration = [];
+temperature = [];
+
+for i = 11:20
+    names(i-10) = 0;
+    pressure(i-10) = F0_PVT_500(i).pressure;
+    vibration(i-10) = F0_PVT_500(i).vibration;
+    temperature(i-10) = F0_PVT_500(i).temperature;
+end
+for i = 21:30
+    names(i-10) = 1;
+    pressure(i-10) = F0_PVT_500(i).pressure;
+    vibration(i-10) = F0_PVT_500(i).vibration;
+    temperature(i-10) = F0_PVT_500(i).temperature;
+end
+
+% 1 - Split the training data in terms of different pairs of variables, perform LDA and plot
+
+% LDA pressure-vibration
+PV = [pressure;vibration]';
+MdlLinearPV = fitcdiscr(PV,names);
+K_PV = MdlLinearPV.Coeffs(1,2).Const;  
+L_PV = MdlLinearPV.Coeffs(1,2).Linear;
+
+% LDA pressure-temperature
+PT = [pressure;temperature]';
+MdlLinearPT = fitcdiscr(PT,names);
+K_PT = MdlLinearPT.Coeffs(1,2).Const;  
+L_PT = MdlLinearPT.Coeffs(1,2).Linear;
+
+% LDA temperature-vibration
+TV = [temperature;vibration]';
+MdlLinearTV = fitcdiscr(TV,names);
+K_TV = MdlLinearTV.Coeffs(1,2).Const;  
+L_TV = MdlLinearTV.Coeffs(1,2).Linear;
+
+% plot pressure-vibration
+figure
+gscatter(PV(:,1),PV(:,2),names)
+hold on
+xlabel('pressure')
+ylabel('vibration')
+legend('black foam','car sponge')
+f_PV = @(x1,x2) K_PV + L_PV(1)*x1 + L_PV(2)*x2;
+fimplicit(f_PV);
+
+% plot pressure-temperature
+figure
+gscatter(PT(:,1),PT(:,2),names)
+hold on
+xlabel('pressure')
+ylabel('temperature')
+legend('black foam','car sponge')
+f_PT = @(x1,x2) K_PT + L_PT(1)*x1 + L_PT(2)*x2;
+fimplicit(f_PT);
+
+% plot temperature-vibration
+figure
+gscatter(TV(:,1),TV(:,2),names)
+hold on
+xlabel('temperature')
+ylabel('vibration')
+legend('black foam','car sponge')
+f_TV = @(x1,x2) K_TV + L_TV(1)*x1 + L_TV(2)*x2;
+fimplicit(f_TV);
+
+%% 2 - Apply LDA to 3D data
+
+% LDA pressure-vibration-temperature
+PVT = [pressure;vibration;temperature]';
+MdlLinearPVT = fitcdiscr(PVT,names);
+K_PVT = MdlLinearPVT.Coeffs(1,2).Const;  
+L_PVT = MdlLinearPVT.Coeffs(1,2).Linear;
+
+% plot pressure-vibration-temperature
+colors = 'rgb';
+figure
+scatter3(PVT(1:10,1),PVT(1:10,2),PVT(1:10,3),colors(1),'filled')
+hold on
+scatter3(PVT(11:20,1),PVT(11:20,2),PVT(11:20,3),colors(2),'filled')
+grid on
+xlabel('pressure')
+ylabel('vibration')
+zlabel('temperature')
+hold on
+f_PVT = @(x1,x2) K_PVT + L_PVT(1)*x1 + L_PVT(2)*x2 + 2495;
+fsurf(f_PVT,'FaceColor',[0 0 1],'EdgeColor','none');
+alpha 0.5
 
 %% Section D - Clustering
